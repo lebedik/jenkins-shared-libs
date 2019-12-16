c65c48bf0598e79ab0ba4172dc61b955a3e83287
Yauheni Lebedzik <yauheni_lebedzik@epam.com>
2019-12-16 15:56:37 +0300

    Add external libs ref

---
+++ vars/tagAWS.groovy
@@ -0,0 +1,36 @@
+/***********************************
+ tagAWS DSL
+ Tags an AMI
+ example usage
+ tagAWS region: 'ap-southeast-2',  ami: 'xyz', tags: ['status':'verifed']
+ ************************************/
+ @Grab(group='com.amazonaws', module='aws-java-sdk-ec2', version='1.11.693')
+
+ import com.amazonaws.services.ec2.*
+ import com.amazonaws.services.ec2.model.*
+ import com.amazonaws.regions.*
+
+ def call(body) {
+   def config = body
+   node {
+     println config
+     addTags(config.region, config.ami, config.tags)
+   }
+}
+
+@NonCPS
+def addTags(region, ami, tags) {
+  def ec2 = AmazonEC2ClientBuilder.standard()
+    .withRegion(region)
+    .build()
+
+  def newTags = []
+  tags.each { key, value ->
+    newTags << new Tag(key, value)
+  }
+
+  ec2.createTags(new CreateTagsRequest()
+    .withResources(ami)
+    .withTags(newTags)
+  )
+}
